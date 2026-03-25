44391c86d3125350e1de88bf4be228877cb7ecfc
EricJohnPena <eric.john.pena@gmail.com>
2026-03-25 12:56:56 +0800

    task2

---
+++ chapter2/task2.java
@@ -0,0 +1,17 @@
+public class task2 {
+public static void main(String[] args){
+	byte zero = 0;	
+	byte one = 1;
+	int two = 2;
+	byte three = 3;
+	char w = 'W';
+	float a = 2.0f;
+	boolean bool = true;
+
+	String output ="H" + three + one + one + zero + " " + w + zero+ "r" + one+"d "+ a+ " "+bool ;
+
+System.out.println(output);
+
+}
+
+}
