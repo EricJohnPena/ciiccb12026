1a3683ff9de8e71677f55375d41f2ed56f2d8252
EricJohnPena <119671372+EricJohnPena@users.noreply.github.com>
2026-03-28 05:21:12 +0000

    day 19 - 28

---
+++ test/Main.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter three numbers: ");
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = scanner.nextInt();
+
+        
+        if(a==b && b==c)System.out.println("All numbers are equal");
+        else{
+            int last_num = a;
+            if(b>last_num)last_num = b;
+            if(c>last_num)last_num = c;
+
+            System.out.println("The largest number is:" + last_num);
+        } 
+            
+        
+
+    //    String result = (a==b && b==c) ? "All numbers are equal." :
+    //                      int last_num = a
+    //                     (b>last_num)?last_num=b
+    //                     (c>last_num)?last_num=c
+    //                     ("The largest number is:" + last_num); 
+    //     System.out.println(result);
+  
+   
+    }
+}
+
