d62ffdd12d063dfb247001a8335a2bdd905ca068
EricJohnPena <119671372+EricJohnPena@users.noreply.github.com>
2026-04-06 04:40:33 +0000

    task3

---
+++ chapter2/task3.java
@@ -0,0 +1,9 @@
+public class task3{
+    public static void main(String[] args){
+        String a = new String("Wow");
+        String b = "Wow";
+
+        boolean b1 = a==b;
+        if(b1)System.out.println("Success");
+    }
+}
