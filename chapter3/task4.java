c8d53ff8c3c93da0977fbc9e60aafe0272c7f587
EricJohnPena <119671372+EricJohnPena@users.noreply.github.com>
2026-04-07 04:08:57 +0000

    Task 4

--- chapter3/task4.java
+++ chapter3/task4.java
@@ -7,16 +7,26 @@ public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
     System.out.println("Input a valid string: ");
     String input = scanner.next();
-StringBuilder str = new StringBuilder(input);
+
 scanner.close();
-isAPalindrome(str);
+isAPalindrome(input);
+//IsAPalindrome(input);
 }
 
-  static void isAPalindrome(StringBuilder str){
-        String original = str.toString();
-        String reverseStr = str.reverse().toString();
-        if (reverseStr.equals(original)) System.out.println("The input string is a palindrome");
+  static void isAPalindrome(String word){
+        StringBuilder sb = new StringBuilder(word);
+        String reverseStr = sb.reverse().toString();
+        if (reverseStr.equals(word)) System.out.println("The input string is a palindrome");
         else System.out.println("The input string is not a palindrome");
+
+
+    }
+
+    ///////////////////////////ternary////////////////////////////////////////
+    static void IsAPalindrome(String word){
+        StringBuilder sb = new StringBuilder(word);
+	String result = (sb.toString().equalsIgnoreCase(sb.reverse().toString())?"Is a palindrome":"Is not a palindrome");
+	System.out.println(result);
     }
     
 }
